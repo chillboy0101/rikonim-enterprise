0332a73e8f0a7acf5c44aed16314624c2021e74d
chillboy0101 <equalizerjr@gmail.com>
2026-02-01 18:49:12 +0000

    Clean up ESLint warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
 import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
 import nextTypescript from 'eslint-config-next/typescript';
 
-export default [
+const config = [
   {
-    ignores: ['.next/**', 'node_modules/**', 'out/**']
+    ignores: ['.next/**', 'node_modules/**', 'out/**', 'scripts/**']
   },
   ...nextCoreWebVitals,
   ...nextTypescript,
   {
     rules: {
+      '@next/next/no-img-element': 'off',
       'react-hooks/purity': 'off',
       'react-hooks/set-state-in-effect': 'off'
     }
   }
 ];
+
+export default config;
