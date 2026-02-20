cabcff3c320acddd010498a4504fc2519dfd0259
chillboy0101 <equalizerjr@gmail.com>
2026-02-20 15:46:03 +0000

    Set Sanity Studio apiVersion

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,12 +6,14 @@ import { structure } from './sanity/deskStructure';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
 export default defineConfig({
   name: 'default',
   title: 'Rikonim Enterprise',
   projectId: projectId || '',
   dataset: dataset || 'production',
+  apiVersion: apiVersion || '2025-01-01',
   basePath: '/studio',
   plugins: [deskTool({ structure }), visionTool()],
   schema: {
