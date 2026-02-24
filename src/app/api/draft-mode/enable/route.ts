7ce3875c1b283d4cdc140add345ad08d3ac8321f
chillboy0101 <equalizerjr@gmail.com>
2026-02-24 15:45:30 +0000

    fix(preview): set preview cookies for iframe

--- src/app/api/draft-mode/enable/route.ts
+++ src/app/api/draft-mode/enable/route.ts
@@ -10,10 +10,20 @@ export async function GET(request: Request) {
   const isSecure = process.env.NODE_ENV === 'production';
   const cookieStore = await cookies();
   const bypass = cookieStore.get('__prerender_bypass');
+  const previewData = cookieStore.get('__next_preview_data');
 
   cookieStore.set({
     name: '__prerender_bypass',
     value: bypass?.value ?? '',
+    httpOnly: true,
+    path: '/',
+    secure: isSecure,
+    sameSite: isSecure ? 'none' : 'lax'
+  });
+
+  cookieStore.set({
+    name: '__next_preview_data',
+    value: previewData?.value ?? '',
     httpOnly: true,
     path: '/',
     secure: isSecure,
