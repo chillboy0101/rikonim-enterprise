1025e259e14db621aa2e3dcb6d7cf0802caeebe8
chillboy0101 <equalizerjr@gmail.com>
2026-02-24 03:56:15 +0000

    Fix Studio CLI config defaults

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,11 +1,11 @@
 import { defineCliConfig } from 'sanity/cli';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'oy93rufy';
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 
 export default defineCliConfig({
   api: {
-    projectId: projectId || '',
-    dataset: dataset || 'production'
+    projectId,
+    dataset
   }
 });
